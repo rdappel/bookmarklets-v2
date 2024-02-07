ee719f6315f7adc8e49ee30b1785862502328016
Ryan Appel <appel@fvtc.edu>
2024-02-07 04:10:27 -0600

    modified full credit script

--- full-credit/script.js
+++ full-credit/script.js
@@ -1,1 +1,11 @@
-javascript:document.querySelector("#currentAttempt_grade").value=document.querySelector("#currentAttempt_pointsPossible").innerText.slice(1),document.querySelector("#currentAttempt_submitButton").dispatchEvent(new Event("click"));
+(async () => {
+
+	const key = 'auto-submit'
+	const autoSubmit = localStorage.getItem(key) || 'true'
+	localStorage.setItem(key, autoSubmit)
+	
+	const maxPoints = document.querySelector('#currentAttempt_pointsPossible').textContent.slice(1)
+    document.querySelector('#currentAttempt_grade').value = maxPoints
+    if (autoSubmit) document.querySelector('#currentAttempt_submitButton').dispatchEvent(new Event('click'))
+	
+})()
