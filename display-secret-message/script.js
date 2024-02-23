d133d567aca7e9a56d55cb77a7f8a32477789712
Ryan Appel <appel@fvtc.edu>
2024-02-22 22:12:25 -0600

    make period optional

--- display-secret-message/script.js
+++ display-secret-message/script.js
@@ -1,5 +1,5 @@
 (async () => {
 
-    alert(localStorage.getItem(`It's a secret to everybody.`) || `No messge found.`)
+    alert(localStorage.getItem(`It's a secret to everybody.`) || localStorage.getItem(`It's a secret to everybody`) || `No messge found.`)
 
 })()
