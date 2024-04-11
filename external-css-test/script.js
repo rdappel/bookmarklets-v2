0201a940f342b111656020192652b88146b90364
Ryan Appel <rdappel@gmail.com>
2024-04-11 13:14:45 -0500

    trying to apply style

--- external-css-test/script.js
+++ external-css-test/script.js
@@ -1,9 +1,10 @@
 (async () => {
 
     const response = await fetch('https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css')
-    const data = await response.text()
 
-    console.log(data)
+    const style = document.createElement('style')
+    style.innerHTML = await response.text()
 
-
+    document.head.appendChild(style)
+    
 })()
